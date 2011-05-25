2c782969e3e5eb500d27bc731b2e98ef3f1bf12d
Osman Üngür <osmanungur@gmail.com>
2011-05-26 00:55:08 +0300

    Docblocks updated for phpDocumentor conventions

--- lib/Heatbeat/Cli/Update.php
+++ lib/Heatbeat/Cli/Update.php
@@ -1,24 +1,43 @@
 <?php
 
+/**
+ * Copyright 2011 Osman Ungur
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ *
+ * @category    Heatbeat
+ * @package     Heatbeat\Cli
+ * @author      Osman Ungur <osmanungur@gmail.com>
+ * @copyright   2011 Osman Ungur
+ * @license     http://www.apache.org/licenses/LICENSE-2.0
+ * @link        http://github.com/import/heatbeat
+ */
+
 namespace Heatbeat\Cli;
 
-/**
- * Update
- *
- * Callback for update cli command
- * 
- * @package    Heatbeat
- * @author     Osman Üngür <osmanungur@gmail.com>
- * @copyright  2011 Osman Üngür
- * @license    http://www.opensource.org/licenses/bsd-license.php BSD License
- * @link       http://github.com/import/heatbeat
- */
 use Symfony\Component\Console\Input\InputArgument,
     Symfony\Component\Console\Input\InputOption,
     Symfony\Component\Console,
     Symfony\Component\Console\Input\InputInterface,
     Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * Callback for CLI Tool update command
+ *
+ * @category    Heatbeat
+ * @package     Heatbeat\Cli
+ * @author      Osman Ungur <osmanungur@gmail.com>
+ */
 class Update extends Console\Command\Command {
 
     public function configure() {
