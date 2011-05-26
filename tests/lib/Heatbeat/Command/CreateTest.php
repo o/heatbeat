48133825634389b36bf4628735530a2e32b2f61f
Osman Üngür <osmanungur@gmail.com>
2011-05-27 01:04:44 +0300

    Tests updated

--- tests/lib/Heatbeat/Command/CreateTest.php
+++ tests/lib/Heatbeat/Command/CreateTest.php
@@ -32,47 +32,4 @@ class CreateTest extends \PHPUnit_Framework_TestCase {
         $command->setStep('foo');
     }
 
-    public function complexArgumentsProvider() {
-        return array(
-            array(
-                'test.rrd',
-                600,
-                array(
-                    'DS:speed:COUNTER:600:U:U'
-                ),
-                array(
-                    'RRA:AVERAGE:0.5:1:24',
-                    'RRA:AVERAGE:0.5:6:10'
-                ),
-                "rrdtool create --step '600' 'test.rrd' 'DS:speed:COUNTER:600:U:U' 'RRA:AVERAGE:0.5:1:24' 'RRA:AVERAGE:0.5:6:10'"
-            ),
-            array(
-                'filename.rrd',
-                300,
-                array(
-                    'foo'
-                ),
-                array(
-                    'bar',
-                    'baz'
-                ),
-                "rrdtool create --step '300' 'filename.rrd' 'foo' 'bar' 'baz'"
-            )            
-        );
-    }
-
-    /**
-     * @dataProvider complexArgumentsProvider
-     */
-    public function testComplexArguments($filename, $step, $datastores, $rras, $result) {
-        $command = new Create();
-        $command->setFilename($filename);
-        $command->setStep($step);
-        $command->setDatastores($datastores);
-        $command->setRras($rras);
-        $this->assertEquals($command->prepare(), $result);
-    }
-
 }
-
-?>
---
+++ tests/lib/Heatbeat/Command/UpdateTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Heatbeat\Command;
+
+/**
+ * Test class for Update.
+ */
+class UpdateTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @dataProvider testSetValuesDataProvider
+     */
+    public function testSetValues($time, $values) {
+        $command = new Update;
+        $this->assertTrue($command->setValues($time, $values));
+    }
+
+    public function testSetValuesDataProvider() {
+        return array(
+            array('Now', array(9, 4, 3, 5)),
+            array('1 hour ago', array(1.2, 3.4, 5.7)),
+            array(1306445720, array(55, 60)),
+            array(1306273330, array(10))
+        );
+    }
+
+}
+
+?>
