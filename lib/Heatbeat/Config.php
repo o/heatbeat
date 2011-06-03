192712cea527ee03070d34d3f290fd0357f81334
Osman Üngür <osmanungur@gmail.com>
2011-06-03 18:01:23 +0300

    Interface for config file

---
+++ lib/Heatbeat/Config.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Copyright 2011 Osman Ungur
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ *
+ * @category    Heatbeat
+ * @package     Heatbeat
+ * @author      Osman Ungur <osmanungur@gmail.com>
+ * @copyright   2011 Osman Ungur
+ * @license     http://www.apache.org/licenses/LICENSE-2.0
+ * @link        http://github.com/import/heatbeat
+ */
+
+namespace Heatbeat;
+
+/**
+ * Config file parser
+ *
+ * @category    Heatbeat
+ * @package     Heatbeat
+ * @author      Osman Ungur <osmanungur@gmail.com>
+ */
+class Config extends Parser implements \IteratorAggregate {
+    const FILENAME = 'config';
+    const NODENAME = 'templates';
+    private $values;
+    
+    public function __construct() {
+        parent::__construct();
+        $this->setFilename(self::FILENAME);
+        $this->setValues($this->parse());
+    }
+
+    protected function setFilePath() {
+        $this->filepath = Autoloader::getInstance()->getPath(Autoloader::FOLDER_ROOT);
+    }
+    
+    public function getValues() {
+        return $this->values;
+    }
+
+    private function setValues($values) {
+        $this->values = $values;
+    }
+    
+    public function getIterator() {
+        return new \ArrayIterator($this->getValues());
+    }
+
+}
