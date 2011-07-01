8e73a9110e153346c088550ad5afa8a594d9fae2
Osman Üngür <osmanungur@gmail.com>
2011-07-01 14:45:18 +0300

    Revert "Removed setting environment variables before execution"
    This reverts commit ad49b07c5e9faf3d1ca82678cf1bbb5639025932.

--- lib/Heatbeat/Util/CommandExecutor.php
+++ lib/Heatbeat/Util/CommandExecutor.php
@@ -117,6 +117,7 @@ class CommandExecutor {
      */
     public function execute() {
         $process = new Process($this->getCommandString());
+        $process->setEnv(explode(PATH_SEPARATOR, \getenv('PATH')));
         $process->run();
         if (!$process->isSuccessful()) {
             throw new ExecutionException($process->getErrorOutput());
