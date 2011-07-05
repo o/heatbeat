1cb52989a0792fd53268d15e07e5bc191d8bfd95
Osman Üngür <osmanungur@gmail.com>
2011-07-05 18:30:08 +0300

    New test added

---
+++ tests/lib/Heatbeat/Source/AbstractInputOutputTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Heatbeat\Source;
+
+/**
+ * Test class for AbstractInputOutput.
+ */
+class AbstractInputOutputTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @var AbstractInputOutput
+     */
+    protected $object;
+
+    protected function setUp() {
+        $this->object = $this->getMockForAbstractClass('Heatbeat\Source\AbstractInputOutput');
+    }
+
+    public function testInitObject() {
+        $this->assertEmpty(iterator_to_array($this->object));
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testSetGetValue($index, $newval) {
+        $this->object->setValue($index, $newval);
+        $this->assertArrayHasKey($index, iterator_to_array($this->object));
+        $this->assertEquals($this->object->getValue($index), $newval);
+    }
+
+    public function dataProvider() {
+        return array(
+            array('foo', 'bar'),
+            array(1, 2),
+            array('go', 'to')
+        );
+    }
+
+}
+
+?>
