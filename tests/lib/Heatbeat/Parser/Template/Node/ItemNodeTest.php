c4986a1e2dc5a63b5c1ee4b5d1221987e8287d05
Osman Üngür <osmanungur@gmail.com>
2011-07-08 14:50:32 +0300

    Item node test updated

--- tests/lib/Heatbeat/Parser/Template/Node/ItemNodeTest.php
+++ tests/lib/Heatbeat/Parser/Template/Node/ItemNodeTest.php
@@ -7,98 +7,63 @@ namespace Heatbeat\Parser\Template\Node;
  */
 class ItemNodeTest extends \PHPUnit_Framework_TestCase {
 
-    private $validationData;
-
-    protected function setUp() {
-        $this->validationData = array(
-            'type' => 'AREA',
-            'definition-name' => 'ds0',
-            'color' => 'FFF000',
-            'legend' => 'Oh my..'
-        );
+    /**
+     * @dataProvider validDataProvider
+     */
+    public function testGetAsString($array, $result) {
+        $object = new ItemNode($array);
+        $this->assertArrayHasKey('type', $array);
+        $this->assertArrayHasKey('definition-name', $array);
+        $this->assertArrayHasKey('color', $array);
+        $this->assertArrayHasKey('legend', $array);
+        $this->assertEquals($result, $object->getAsString());
+        $this->assertInternalType('string', $object->getAsString());
     }
 
     /**
-     * @dataProvider itemDataProvider
+     * @dataProvider validDataProvider
      */
-    public function testGetAsString($type, $definitionname, $color, $legend, $result) {
-        $object = new ItemNode(array(
-                    'type' => $type,
-                    'definition-name' => $definitionname,
-                    'color' => $color,
-                    'legend' => $legend
-                ));
-        $this->assertEquals($result, $object->getAsString());
+    public function testValidate($array) {
+        $object = new ItemNode($array);
+        $this->assertInternalType('array', $array);
+        $this->assertArrayHasKey('type', $array);
+        $this->assertArrayHasKey('definition-name', $array);
+        $this->assertArrayHasKey('color', $array);
+        $this->assertArrayHasKey('legend', $array);
         $this->assertTrue($object->validate());
     }
 
-    public function itemDataProvider() {
+    /**
+     * @expectedException Heatbeat\Exception\NodeValidationException
+     * @dataProvider nonValidDataProvider
+     */
+    public function testFailValidate($array) {
+        $object = new ItemNode($array);
+        $this->assertInternalType('array', $array);
+        $object->validate();
+    }
+
+    public function validDataProvider() {
         return array(
-            array('LINE', 'ds1', 'FFFFFF', 'Test Legend', 'LINE:ds1#FFFFFF:Test Legend'),
-            array('AREA', 'ds0c', 'FF00FF', 'resolution 7200 seconds per interval', 'AREA:ds0c#FF00FF:resolution 7200 seconds per interval'),
-            array('STACK', 'ds2', 'FF00FA', 'moo', 'STACK:ds2#FF00FA:moo'),
-            array('LINE2', 'meh', 'FF00FC', 'bars', 'LINE2:meh#FF00FC:bars')
+            array(array('type' => 'AREA', 'definition-name' => 'ds1', 'color' => 'F1BE4F', 'legend' => 'Datastore 1'), 'AREA:ds1#F1BE4F:Datastore 1'),
+            array(array('type' => 'LINE', 'definition-name' => 'ds2', 'color' => 'EE9A37', 'legend' => 'Datastore 2'), 'LINE:ds2#EE9A37:Datastore 2'),
+            array(array('type' => 'LINE1', 'definition-name' => 'dd3', 'color' => 'FF339A', 'legend' => 'DD 3'), 'LINE1:dd3#FF339A:DD 3'),
+            array(array('type' => 'LINE2', 'definition-name' => 'files', 'color' => 'E71966', 'legend' => 'Filez'), 'LINE2:files#E71966:Filez'),
+            array(array('type' => 'LINE3', 'definition-name' => 'temps', 'color' => 'D90A51', 'legend' => 'Temperatures'), 'LINE3:temps#D90A51:Temperatures'),
+            array(array('type' => 'STACK', 'definition-name' => 'users', 'color' => '000000', 'legend' => 'Users'), 'STACK:users#000000:Users'),
         );
     }
 
-    /**
-     * @expectedException Heatbeat\Exception\NodeValidationException
-     */
-    public function testTypeNotExists() {
-        $array = $this->validationData;
-        unset($array['type']);
-        $object = new ItemNode($array);
-        $object->validate();
-    }
-
-    /**
-     * @expectedException Heatbeat\Exception\NodeValidationException
-     */
-    public function testDefinitionNotExists() {
-        $array = $this->validationData;
-        unset($array['definition-name']);
-        $object = new ItemNode($array);
-        $object->validate();
-    }
-
-    /**
-     * @expectedException Heatbeat\Exception\NodeValidationException
-     */
-    public function testColorNotExists() {
-        $array = $this->validationData;
-        unset($array['color']);
-        $object = new ItemNode($array);
-        $object->validate();
-    }
-
-    /**
-     * @expectedException Heatbeat\Exception\NodeValidationException
-     */
-    public function testLegendNotExists() {
-        $array = $this->validationData;
-        unset($array['legend']);
-        $object = new ItemNode($array);
-        $object->validate();
-    }
-
-    /**
-     * @expectedException Heatbeat\Exception\NodeValidationException
-     */
-    public function testInvalidType() {
-        $array = $this->validationData;
-        $array['type'] = 'FOO';
-        $object = new ItemNode($array);
-        $object->validate();
-    }
-
-    /**
-     * @expectedException Heatbeat\Exception\NodeValidationException
-     */
-    public function testInvalidColor() {
-        $array = $this->validationData;
-        $array['color'] = 'FFFFFZ';
-        $object = new ItemNode($array);
-        $object->validate();
+    public function nonValidDataProvider() {
+        return array(
+            array(array('type' => 'AREA', 'definitionname' => 'ds1', 'color' => 'F1BE4F', 'legend' => 'Datastore 1')),
+            array(array('type' => 'LINE', 'definition-name' => 'ds2!', 'color' => 'EE9A37', 'legend' => 'Datastore 2')),
+            array(array('type' => 'LINE5', 'definition-name' => 'dd3', 'color' => 'FF339A', 'legend' => 'DD 3')),
+            array(array('type' => 'LINE10', 'definition-name' => 'files', 'color' => 'E71966', 'legend' => 'Filez')),
+            array(array('type' => 'LINE3', 'definition-name' => 'temps', 'color' => 'D90A51', 'legend' => '')),
+            array(array('type' => 'STACK', 'legend' => 'Users'), 'STACK:users#000000:Users'),
+            array(array('type' => 'STACK', 'definition-name' => 'users', 'legend' => 'Users'))
+        );
     }
 
 }
