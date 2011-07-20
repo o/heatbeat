c7d24255ca443d20e1d222032827608f58fe7990
Osman Üngür <osmanungur@gmail.com>
2011-07-20 13:58:27 +0300

    Fixed tests

--- tests/lib/Heatbeat/Parser/Template/Node/TemplateOptionNodeTest.php
+++ tests/lib/Heatbeat/Parser/Template/Node/TemplateOptionNodeTest.php
@@ -26,22 +26,22 @@ class TemplateOptionNodeTest extends \PHPUnit_Framework_TestCase {
 
     public function validDataProvider() {
         return array(
-            array(array('name' => 'Loads', 'version' => 0.1, 'source-name' => 'Foo_Random')),
-            array(array('name' => 'Randoms', 'version' => 1, 'source-name' => 'System_Load')),
-            array(array('name' => 'Foo', 'version' => 2, 'source-name' => 'Foo_Random')),
-            array(array('name' => 'Baz data', 'version' => 0.2, 'source-name' => 'Foo_Random')),
-            array(array('name' => 'No name', 'version' => 0.5, 'source-name' => 'Foo_Random'))
+            array(array('name' => 'Loads', 'version' => 0.1, 'source-name' => 'Test_Random')),
+            array(array('name' => 'Randoms', 'version' => 1, 'source-name' => 'Test_Random')),
+            array(array('name' => 'Foo', 'version' => 2, 'source-name' => 'Test_Random')),
+            array(array('name' => 'Baz data', 'version' => 0.2, 'source-name' => 'Test_Random')),
+            array(array('name' => 'No name', 'version' => 0.5, 'source-name' => 'Test_Random'))
         );
     }
 
     public function nonValidDataProvider() {
         return array(
-            array(array('name' => '', 'version' => 0.1, 'source-name' => 'Foo_Random')),
+            array(array('name' => '', 'version' => 0.1, 'source-name' => 'Test_Random')),
             array(array('name' => 'Randoms', 'version' => '', 'source-name' => 'System_Load')),
             array(array('name' => 'Foo', 'version' => 2, 'source-name' => '')),
-            array(array('names' => 'Baz data', 'version' => 0.2, 'source-name' => 'Foo_Random')),
-            array(array('name' => 'No name', 'versions' => 0.5, 'source-name' => 'Foo_Random')),
-            array(array('name' => 'No name', 'version' => 0.5, 'sourcename' => 'Foo_Random')),
+            array(array('names' => 'Baz data', 'version' => 0.2, 'source-name' => 'Test_Random')),
+            array(array('name' => 'No name', 'versions' => 0.5, 'source-name' => 'Test_Random')),
+            array(array('name' => 'No name', 'version' => 0.5, 'sourcename' => 'Test_Random')),
             array(array('name' => 'Randoms', 'version' => 1, 'source-name' => 'System_Loads'))
         );
     }
