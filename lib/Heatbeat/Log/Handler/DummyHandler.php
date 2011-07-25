75f8958e37eb702e8b51fb71f1e96d50f9977eb3
Osman Üngür <osmanungur@gmail.com>
2011-07-25 07:37:58 +0300

    Implemented dummy handler

---
+++ lib/Heatbeat/Log/Handler/DummyHandler.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Copyright 2011 Osman Ungur
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ *
+ * @category    Heatbeat
+ * @package     Heatbeat\Log\Handler
+ * @author      Osman Ungur <osmanungur@gmail.com>
+ * @copyright   2011 Osman Ungur
+ * @license     http://www.apache.org/licenses/LICENSE-2.0
+ * @link        http://github.com/import/heatbeat
+ */
+
+namespace Heatbeat\Log\Handler;
+
+use Heatbeat\Autoloader;
+
+/**
+ * Dummy handler, logs nothing
+ *
+ * @category    Heatbeat
+ * @package     Heatbeat\Log\Handler
+ * @author      Osman Ungur <osmanungur@gmail.com>
+ */
+class DummyHandler extends AbstractLogHandler implements LogHandlerInterface {
+
+    private function handle($message) {
+        return true;
+    }
+
+    private function isHandling() {
+        return true;
+    }
+
+    private function format($message) {
+        return $message;
+    }
+
+}
