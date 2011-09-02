5a56fd46f8d6855349af2bc8054af737987ae24a
Osman Üngür <osmanungur@gmail.com>
2011-09-02 16:51:24 +0300

    testCurrentWithMultipleNode and added key() tests

--- tests/lib/Heatbeat/Definition/Template/RraDefinitionTest.php
+++ tests/lib/Heatbeat/Definition/Template/RraDefinitionTest.php
@@ -8,24 +8,38 @@ namespace Heatbeat\Definition\Template;
 class RraDefinitionTest extends \PHPUnit_Framework_TestCase {
 
     public function testCurrent() {
-        $definition = new RraDefinition(
+        $object = new RraDefinition(
                         array(array('cf' => 'AVERAGE', 'xff' => 0.8, 'steps' => 2, 'rows' => 68))
         );
-        foreach ($definition as $value) {
+        foreach ($object as $value) {
             $this->assertInstanceOf('\Heatbeat\Parser\Template\Node\RraNode', $value);
         }
+        $this->assertEquals(1, $object->key());
     }
 
     public function testCurrentWithMultipleCF() {
-        $definition = new RraDefinition(
+        $object = new RraDefinition(
                         array(array('cf' => array('AVERAGE', 'LAST'), 'xff' => 0.5, 'steps' => 4, 'rows' => 100))
         );
-        foreach ($definition as $value) {
+        foreach ($object as $value) {
             $this->assertInstanceOf('\Heatbeat\Parser\Template\Node\RraNode', $value);
         }
+        $this->assertEquals(3, $object->key());
     }
-    
-    
+
+    public function testCurrentWithMultipleNode() {
+        $object = new RraDefinition(
+                        array(
+                            array('cf' => 'LAST', 'xff' => 0.5, 'steps' => 6, 'rows' => 500),
+                            array('cf' => array('MAX', 'AVERAGE'), 'xff' => 0.2, 'steps' => 12, 'rows' => 720)
+                        )
+        );
+        foreach ($object as $value) {
+            $this->assertInstanceOf('\Heatbeat\Parser\Template\Node\RraNode', $value);
+        }
+        $this->assertEquals(4, $object->key());
+    }
+
 }
 
 ?>
