ac493b652803957f9e0a4eac939d4579ad1eca0c
Osman Üngür <osmanungur@gmail.com>
2011-09-02 16:50:58 +0300

    testCurrentWithMultipleNode

--- tests/lib/Heatbeat/Definition/Template/DatastoreDefinitionTest.php
+++ tests/lib/Heatbeat/Definition/Template/DatastoreDefinitionTest.php
@@ -14,8 +14,21 @@ class DatastoreDefinitionTest extends \PHPUnit_Framework_TestCase {
         foreach ($object as $value) {
             $this->assertInstanceOf('\Heatbeat\Parser\Template\Node\DatastoreNode', $value);
         }
+        $this->assertEquals(1, $object->key());
     }
 
+    public function testCurrentWithMultipleNode() {
+        $object = new DatastoreDefinition(array(
+                    array('name' => 'foo', 'type' => 'GAUGE', 'heartbeat' => 600, 'min' => 0, 'max' => 1),
+                    array('name' => 'bar', 'type' => 'COUNTER', 'heartbeat' => 1200, 'min' => 0, 'max' => 500)
+            ));
+        foreach ($object as $value) {
+            $this->assertInstanceOf('\Heatbeat\Parser\Template\Node\DatastoreNode', $value);
+        }
+        $this->assertEquals(2, $object->key());
+    }
+    
+    
 }
 
 ?>
